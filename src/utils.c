139376a6c5354cb701f64c7ed77717ce7e71e064
Kylian Daumont <kdaumont@student.42.fr>
2024-04-17 13:38:39 +0200

    init parsing

--- src/utils.c
+++ src/utils.c
@@ -6,15 +6,42 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 10:26:39 by cornguye          #+#    #+#             */
-/*   Updated: 2024/04/16 16:52:36 by kdaumont         ###   ########.fr       */
+/*   Updated: 2024/04/17 09:18:10 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+/* Print an error message in the standard error output
+@param str -> message to print
+*/
 void	error_msg(char *str)
 {
 	ft_putstr_fd("Error\n", 2);
 	ft_putstr_fd(str, 2);
 	ft_putchar_fd('\n', 2);
+}
+
+/* Check if the file have the extension given
+@param str -> file where the extension is checked
+@param ext -> file's extension
+@return :
+	0 = bad extension
+	1 = same extension
+*/
+int	is_cub_file(char *str, char *ext)
+{
+	int	i;
+	int	j;
+
+	i = ft_strlen(str) - ft_strlen(ext);
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] != ext[j])
+			return (0);
+		i++;
+		j++;
+	}
+	return (1);
 }
